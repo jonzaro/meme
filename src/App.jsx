3eb0cc230af349024f774f5cf4d5169de68e9430
David Wangen <david.wangen@gmail.com>
2022-05-25 17:47:13 -0500

    finished meme project

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,60 @@
 import logo from "./logo.svg";
 import "./style.css";
-import React from "react";
+import React, {useState} from 'react'
 import Header from "./components/Header";
-import Meme from "./components/Meme";
-import GeneratedMemes from "./components/GeneratedMemes";
+import MemeMaker from "./components/MemeMaker";
+import MemeEdits from "./components/MemeEdits";
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
-  const [things, setThings] = React.useState(["Thing 1", "Thing 2"]);
+
+
+
+function App(props) {
+    const [memeList, setMemeList] = useState([])
+
+    const updateMemeItem = (id, updatedItem) => { 
+      setMemeList(prevMemeList => { 
+          return prevMemeList.map((item)=> { 
+              if (item.id === id) { 
+                  return updatedItem
+              }
+              else {
+                  return item
+              }
+          })
+      })
+  }
+
+  const addNewItem = (newItem) => {
+    newItem.id = uuidv4()
+    setMemeList(prev=>[...prev, newItem])
+  }
+
+  function deleteMemeImage(id) {
+    console.log(id)
+    setMemeList(memeList.filter((meme) => meme.id != id))
+  }
+
+  const list = memeList.map((meme, index) => {
+    return  <MemeEdits
+      key={meme.id}
+      topText={meme.topText}
+      bottomText={meme.bottomText}
+      randomImage={meme.randomImage}
+      updateMemeItem={updateMemeItem}
+      deleteMemeImage={deleteMemeImage}
+      id={meme.id}
+    />})
+
+  
 
   return (
     <div className="App">
       <Header />
-      <Meme />
-      {/* <GeneratedMemes /> */}
+      <MemeMaker submit={addNewItem}/>
+      <h2>Created Memes</h2>
+      <hr/>
+      {list}
     </div>
   );
 }
